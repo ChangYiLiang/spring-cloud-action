2ee9c17155072054b34db17b75f70dc59ca63ed9
chyl <changyiliang@foxmail.com>
2018-10-07 22:37:44 +0800

    添加config配置中心

--- order/src/main/java/com/chyl/order/controller/ConfigTest.java
+++ order/src/main/java/com/chyl/order/controller/ConfigTest.java
@@ -2,6 +2,7 @@ package com.chyl.order.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2018-10-07 下午10:13
  */
 @RestController
+@RequestMapping("config")
 public class ConfigTest {
 
     @Value("${status}")
     private boolean status;
 
-    @GetMapping
+    @GetMapping("status")
     public String getStatus() {
         return status + "";
     }
