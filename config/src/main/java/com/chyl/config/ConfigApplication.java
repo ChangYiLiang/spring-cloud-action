63224c227282ea17b3fab443f5e09473a6fc5507
chyl <changyiliang@foxmail.com>
2018-10-07 21:42:04 +0800

    添加config配置中心

--- config/src/main/java/com/chyl/config/ConfigApplication.java
+++ config/src/main/java/com/chyl/config/ConfigApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigApplication {
 
 	public static void main(String[] args) {
