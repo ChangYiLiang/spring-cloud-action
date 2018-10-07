b220c2ffe780445495c48559c7df4d7954a85a84
chyl <changyiliang@foxmail.com>
2018-10-07 23:34:40 +0800

    添加config配置中心

--- order/src/main/java/com/chyl/order/OrderApplication.java
+++ order/src/main/java/com/chyl/order/OrderApplication.java
@@ -1,14 +1,12 @@
 package com.chyl.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAutoConfiguration
 public class OrderApplication {
 
 	public static void main(String[] args) {
