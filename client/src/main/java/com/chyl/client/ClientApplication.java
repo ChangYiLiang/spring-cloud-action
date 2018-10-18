9543813780cdba6ad3cae7f2804619ee2d7ad0de
chyl <changyiliang@foxmail.com>
2018-10-18 22:25:16 +0800

    添加zipkin

--- client/src/main/java/com/chyl/client/ClientApplication.java
+++ client/src/main/java/com/chyl/client/ClientApplication.java
@@ -3,13 +3,31 @@ package com.chyl.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@RequestMapping(value = "order", method = RequestMethod.GET)
+	public String service1() {
+		return restTemplate().getForObject("http://order/zuul/hello", String.class);
 	}
 }
